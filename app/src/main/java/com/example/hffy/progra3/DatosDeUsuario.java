e500682a97f3e4f1d6e93f31e7df601a97885463
HFFY <hugorilahffy@gmail.com>
2017-02-28 23:42:03 -0400

    Holo

---
+++ app/src/main/java/com/example/hffy/progra3/DatosDeUsuario.java
@@ -0,0 +1,24 @@
+package com.example.hffy.progra3;
+
+/**
+ * Created by HFFY on 23/2/2017.
+ */
+
+public class DatosDeUsuario {
+
+    // TODO : crear algoritmo para guardar datos de usuario mediante matrices o pilas
+
+
+
+    public boolean verificarUsuario(String u, String p){
+
+        // TODO : crear algoritmo para verificar datos de usuario
+        return true;
+    }
+
+
+
+
+
+
+}
